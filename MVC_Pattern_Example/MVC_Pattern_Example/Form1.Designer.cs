949e5db0ac970a08491ada030e3d12c25eaa1c1d
Burak Hamdi TUFAN <43945916+thecodeprogram@users.noreply.github.com>
2020-04-02 19:49:37 +0300

    Add files via upload

---
+++ MVC_Pattern_Example/MVC_Pattern_Example/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MVC_Pattern_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIncrease = new System.Windows.Forms.Button();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIncrease
+            // 
+            this.btnIncrease.Location = new System.Drawing.Point(12, 12);
+            this.btnIncrease.Name = "btnIncrease";
+            this.btnIncrease.Size = new System.Drawing.Size(173, 69);
+            this.btnIncrease.TabIndex = 0;
+            this.btnIncrease.Text = "Increase";
+            this.btnIncrease.UseVisualStyleBackColor = true;
+            this.btnIncrease.Click += new System.EventHandler(this.btnIncrease_Click);
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblValue.Location = new System.Drawing.Point(191, 14);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(408, 67);
+            this.lblValue.TabIndex = 1;
+            this.lblValue.Text = "Thecodeprogram";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 109);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.btnIncrease);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIncrease;
+        private System.Windows.Forms.Label lblValue;
+    }
+}
+
---
+++ MVC_Pattern_Example/MVC_Pattern_Example/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MVC_Pattern_Example
+{
+    public partial class Form1 : Form
+    {
+        int value = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblValue.Text = "Value : " + value.ToString();
+        }
+
+        private void btnIncrease_Click(object sender, EventArgs e)
+        {
+            //Make some operations here and update the model
+            value = (value * 3) + 4;
+            //And update the View through the related model
+            lblValue.Text = "Value : " + value.ToString();
+        }
+    }
+}
